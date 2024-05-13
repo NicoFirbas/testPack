18f61e6513c6ef41c9b20b9e32bd4e17a3ee2a40
Nicolas Firbas <nicodell@pop-os.localdomain>
2024-05-13 21:05:00 +0200

    first commit

---
+++ R/adder.R
@@ -0,0 +1,17 @@
+
+
+
+#' Title
+#'
+#' @param x a number
+#' @param y another number
+#'
+#' @return returns x + y
+#' @export
+#'
+#' @examples adder(2, 3) returns 5
+adder <- function(x, y) {
+
+  return(x+y)
+
+}
