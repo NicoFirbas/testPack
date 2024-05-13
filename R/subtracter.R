a42fb7d349d47e57110f82aa0ed7f713eac1ce7c
Nicolas Firbas <nicodell@pop-os.localdomain>
2024-05-13 21:48:45 +0200

    adding the subtracter function

---
+++ R/subtracter.R
@@ -0,0 +1,19 @@
+#weird secondary function that doesnt do much
+
+
+
+#' Title
+#'
+#' @param x a number
+#' @param y another number
+#'
+#' @return returns x-y
+#' @export
+#'
+#' @examples `subtract(x, y)`
+subtract <- function(x, y) {
+
+  return(x-y)
+
+
+}
